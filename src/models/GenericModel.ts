4d8105c2c5d3e0d8e33ec36d20e0bbbb0941c460
Priscila Silva <priscilacds_100@yahoo.com.br>
2022-07-06 22:36:44 -0300

    feat(Generic Model): Implementação de uma model genérica
    com as cinco funcionalidades básicas: read, readOne, create, update e delete

---
+++ src/models/GenericModel.ts
@@ -0,0 +1,29 @@
+import { Model as M, Document } from 'mongoose';
+import { Model } from '../interfaces/ModelInterface';
+
+abstract class GenericModel<T> implements Model<T> {
+  constructor(protected model: M<T & Document>) { }
+  
+  async create(obj: T): Promise<T> {
+    return this.model.create({ ...obj });
+  }
+
+  async read(): Promise<T[]> {
+    return this.model.find();
+  }
+
+  async readOne(id: string): Promise<T | null> {
+    return this.model.findOne({ id_: id });
+  }
+  
+  async update(id: string, obj: T): Promise<T | null> {
+    return this.model
+      .findOneAndUpdate({ id_: id }, obj, { returnOriginal: false });
+  }
+
+  async delete(id: string): Promise<T | null> {
+    return this.model.findOneAndDelete({ id_: id });
+  }
+}
+
+export default GenericModel;
