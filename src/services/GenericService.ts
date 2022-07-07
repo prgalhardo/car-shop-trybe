df50023c8519ffbd7700c5c6b7b80c4dfcf03d5a
Priscila Silva <priscilacds_100@yahoo.com.br>
2022-07-07 00:07:55 -0300

    feat(Generic Service): Implementação da service genérica
    com os 5 métodos básicos do CRUD

---
+++ src/services/GenericService.ts
@@ -0,0 +1,32 @@
+import { ZodError } from 'zod'; 
+import GenericModel from '../models/GenericModel';
+
+export interface ServiceError {
+  error: ZodError;
+}
+
+abstract class GenericService<T> {
+  constructor(protected model: GenericModel<T>) { }
+
+  public async create(obj: T): Promise<T | ServiceError> {
+    return this.model.create(obj);
+  }
+
+  public async read(): Promise<T[]> {
+    return this.model.read();
+  }
+
+  public async readOne(id: string): Promise<T | null | ServiceError> {
+    return this.model.readOne(id);
+  }
+
+  public async update(id: string, obj: T): Promise<T | null | ServiceError> {
+    return this.model.update(id, obj);
+  }
+
+  public async delete(id: string): Promise<T | null> {
+    return this.model.delete(id);
+  }
+}
+
+export default GenericService;
